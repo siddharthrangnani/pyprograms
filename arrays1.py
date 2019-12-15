d35b53f8799d08ad57ee96f9eab5bba248f89863
siddharthrangnani <47933964+siddharthrangnani@users.noreply.github.com>
2019-12-15 15:34:16 +0530

    new

--- arrays1.py
+++ arrays1.py
@@ -1,5 +1,5 @@
 from array import *
-a=array("u",["a","b","c"])
+a=array("i",[1,2,3,4])
 print("arrays are")
 for array in a:
     print(array)
---
+++ fibonaccci.py
@@ -0,0 +1,15 @@
+n=int(input("how many fibonacci"))
+f1=0
+f2=1
+c=2
+if n==1:
+    print(f1)
+elif n==2:
+    print(f1,"\n",f2,sep="")
+else:
+    print(f1,"\n",f2,sep="")
+    while c<n:
+        f=f1+f2
+        print(f)
+        f1,f2=f2,f
+        c+=1
