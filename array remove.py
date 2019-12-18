77c8cd2f474b1dc45763d5dde70c25989c13656e
siddharthrangnani <47933964+siddharthrangnani@users.noreply.github.com>
2019-12-18 21:03:58 +0530

    nesting array

---
+++ array remove.py
@@ -0,0 +1,8 @@
+from array import *
+arr=array("i",[10,11,12,13,14])
+# 11 is removed from the list
+arr.remove(11)
+for a in arr:
+    {
+        print(a)
+    }
---
+++ array reverse.py
@@ -0,0 +1,5 @@
+from array import *
+arr=array("i",[10,11,12,13,14])
+arr.reverse()
+for a in arr:
+    print(a)
