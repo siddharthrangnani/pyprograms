537b3e67b41a55505665ec99da8d84e6b846b4b3
siddharthrangnani <47933964+siddharthrangnani@users.noreply.github.com>
2020-12-02 09:54:16 +0530

    12/2/2020

--- pytube downloader.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube
-import urllib3
-http = urllib3.PoolManager()
-#input string as url
-urlstring = str(input())
-r = http.request('GET', urlstring)
-yt = YouTube(r)
-print(yt.title)
